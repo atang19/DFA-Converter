72903c934427b1284faaaf87dd54170b57e86238
Andy Tang <andy.tang62@gmail.com>
2017-02-22 23:07:03 -0500

    Finished base and stringtraverse

--- DFAMap.java
+++ DFAMap.java
@@ -1,5 +1,72 @@
+import java.util.*;
+
 public class DFAMap{
 
 	public int numStates;
-	public 
+	public ArrayList<DFAState> stateList;
+	public ArrayList<DFAState> finalStates;
+	public int startingState;
+
+	public DFAMap() {
+		int numStates = 0;
+		stateList = new ArrayList<DFAState>();
+		finalStates = new ArrayList<DFAState>(); 
+		startingState = 0;
+	}
+
+	public void addState(){
+		stateList.add(new DFAState(numStates, false));
+		numStates += 1;
+	}
+
+	public void addFinalState(){
+		stateList.add(new DFAState(numStates, true));
+		finalStates.add(stateList.get(numStates));
+		numStates += 1;
+	}
+
+	public void addEdge(int origin, int dest, String symbols){
+		stateList.get(origin).addEdgeOut(dest, symbols);
+		stateList.get(dest).addEdgeIn(origin, symbols);
+	}
+
+	public void StringTraverse(String s){
+		boolean dead = false;
+		int l = s.length();
+		int currentState = startingState;
+		for(int i=0; i<l; i++){
+			String input = Character.toString(s.charAt(i));
+			if(stateList.get(currentState).edgesOut.containsKey(input) == false){
+				System.out.println("You've encountered a dead state or characters in this string are not in the alphabet.");
+				dead = true;
+				break;
+			}
+			else{
+				currentState = stateList.get(currentState).edgesOut.get(input);
+			}
+		}
+		if(stateList.get(currentState).isFinal && dead == false){
+			System.out.println("The string is accepted in the language.");
+		}
+		else if (dead == false){
+			System.out.println("The string terminates in a non-final state.");
+		}
+		else{
+
+		}
+	}
+
+
+	public void printStates(){
+		for(int i=0; i<stateList.size(); i++){
+			System.out.println(stateList.get(i).stateID + ". Is final: " + stateList.get(i).isFinal);
+		}
+	}
+
+	public void printAllRelations(){
+		for(int i=0; i<stateList.size(); i++){
+			System.out.println("For state " + i + ":");
+			stateList.get(i).printRelations();
+		}
+	}
 }
--- DFAState.java
+++ DFAState.java
@@ -1,6 +1,55 @@
+import java.util.*;
+
 public class DFAState{
 
 	public boolean isFinal;
 	public int stateID;
+	public Map<String, Integer> edgesOut;
+	public ArrayList<Integer> edgesOutList;
+	public Map<String, Integer> edgesIn;
+	public ArrayList<Integer> edgesInList;
+
+
+	public DFAState(int id, boolean iF){
+		stateID = id;
+		isFinal = iF;
+		edgesOut = new HashMap<String, Integer>();
+		edgesOutList = new ArrayList<Integer>();
+		edgesIn = new HashMap<String, Integer>();
+		edgesInList = new ArrayList<Integer>();
+	}
 	
+	public void addEdgeOut(int targetID, String symbols){
+		edgesOut.put(symbols, targetID);
+		edgesOutList.add(targetID);
+	}
+
+	public void addEdgeIn(int targetID, String symbols){
+		edgesIn.put(symbols, targetID);
+		edgesInList.add(targetID);
+	}
+
+	//debugging functions
+
+	public void printRelations(){
+		System.out.println("The state " + stateID + " has the following outward edges:");
+		for ( String key : edgesOut.keySet() ) {
+			String outIDSymbol = key;
+			int outID = edgesOut.get(key);
+			System.out.println("State : " + outID + ", via symbols: " + outIDSymbol);
+
+		}
+			/*
+			int outID = edgesOutList.get(i);
+			String outIDSymbol = edgesOut.get(outID);
+			*/
+		System.out.println("The state " + stateID + " has the following inward edges:");
+		for(String key : edgesIn.keySet()){
+			String inIDSymbol = key;
+			int inID = edgesIn.get(key);
+			System.out.println("State : " + inID + ", via symbols: " + inIDSymbol);
+		}		
+	}
+
+
 }
